532ae8b79cf8e186894b5b3c34bc77c9bee98b96
Vladislav Sumin <vladislavsumin@mail.ru>
2026-09-09 23:59:39 +0300

    Add bevy deps && empty window creation

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -1,1 +1,8 @@
 //! Bevy-клиент (presentation): рендер, ввод, отладка.
+
+use bevy::prelude::*;
+
+/// Запускает игровой клиент.
+pub fn run() {
+    App::new().add_plugins(DefaultPlugins).run();
+}
